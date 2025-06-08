5204156958e5c813d499465b7ce5df51be3998bc
Sebastian Angrick <sebastian.angrick@student.hpi.uni-potsdam.de>
2025-06-08 12:40:56 +0200

    no max-deg selection

--- src/utils.rs
+++ src/utils.rs
@@ -10,14 +10,11 @@ pub fn is_hitting_set(hs: &[NodeIdx], instance: &Instance) -> bool {
 }
 
 pub fn select_vertex(instance: &Instance, vertex_importance: &[f64]) -> NodeIdx {
-    instance
+    *instance
         .nodes()
         .iter()
-        .copied()
-        .max_by(|&a, &b| {
-            let score_a = vertex_importance[a.idx()] * instance.node_degree(a) as f64;
-            let score_b = vertex_importance[b.idx()] * instance.node_degree(b) as f64;
-            score_a.partial_cmp(&score_b).unwrap_or(std::cmp::Ordering::Equal)
+        .max_by(|&a, &b| { 
+            vertex_importance[a.idx()].partial_cmp(&vertex_importance[b.idx()]).unwrap_or(std::cmp::Ordering::Equal)
         })
         .expect("Branching on an empty instance")
 }
